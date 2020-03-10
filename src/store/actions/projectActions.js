a80bae0a408ea0aece1710036ff314ca1b3dd113
Ahmed Afify <a.afify1995@gmail.com>
2020-03-10 18:26:23 +0000

    create project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,23 @@
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
-       dispatch({type: 'CREATE_PROJECT', project}) 
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Ninja',
+            authorLastNAme: 'batat',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project}) 
+
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err}) 
+
+        })
+
+
+
+
 
     }
 }
