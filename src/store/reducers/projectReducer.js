c54ae0e124cee687a34c56c62ec1f7f8c1a43e4c
Ahmed Afify <aafify@dyn192018.shef.ac.uk>
2020-03-12 15:13:48 +0000

    data output

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -11,8 +11,10 @@ const projectReducer = (state = initState, action) => {
     case "CREATE_PROJECT":
       console.log("created project", action.project);
       return state;
+      break;
     case 'CREATE_PROJECT_ERROR':
         console.log('create project error', action.err)
+        break;
     default:
         return state;
   }
