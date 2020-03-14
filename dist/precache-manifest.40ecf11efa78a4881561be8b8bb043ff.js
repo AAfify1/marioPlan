1bbf0685d104741dd0c32332521edcbc400ee903
Ahmed Afify <aafify@pc-194-104.customer.ask4.lan>
2020-03-14 20:20:39 +0000

    Done

---
+++ dist/precache-manifest.40ecf11efa78a4881561be8b8bb043ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7494c0adbdf6d069295a01d0820e15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d02020aefc8f031369b",
+    "url": "/static/css/main.db50ebd9.chunk.css"
+  },
+  {
+    "revision": "3609c66b18b2a18d1174",
+    "url": "/static/js/2.5e16a5f7.chunk.js"
+  },
+  {
+    "revision": "4691121c96f91c7f8618b1ecc9480cfc",
+    "url": "/static/js/2.5e16a5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d02020aefc8f031369b",
+    "url": "/static/js/main.9f16fe55.chunk.js"
+  },
+  {
+    "revision": "c80bae48cdf384b85e9c",
+    "url": "/static/js/runtime-main.7551f220.js"
+  }
+]);
